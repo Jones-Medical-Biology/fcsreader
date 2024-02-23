05f89301577b59f20b10c4a5a851032787c314de
McCarthy <cmccarthy3@ufl.edu>
2024-02-23 10:00:25 -0500

    now can read file

--- src/MyLib.hs
+++ src/MyLib.hs
@@ -1,4 +1,29 @@
-module MyLib (someFunc) where
+module MyLib (someFunc, runHCat) where
+
+import qualified System.Environment as Env
+
 
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
+
+runHCat :: IO ()
+runHCat = handleArgs >>= displayMessage
+  where
+   displayMessage parsedArgument =
+    case parsedArgument of
+     Left errMessage ->
+      putStrLn $ "Error: " <> errMessage
+     Right filename ->
+      readFile filename >>= putStrLn
+
+handleArgs :: IO (Either String FilePath)
+handleArgs =
+  parseArgs <$> Env.getArgs
+  where
+   parseArgs argumentList =
+    case argumentList of
+     [fname] -> Right fname
+     [] -> Left "Error: No arguments provided!"
+     _ -> Left "garbage detected"
+
+
