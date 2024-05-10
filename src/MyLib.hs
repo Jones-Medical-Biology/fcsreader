8b210cb0f9af9956c93b2c3b7eb708a719605b49
Noah Thomas Jones <noahtjones@gmail.com>
2024-05-10 15:47:10 -0400

    added fcs file fields

--- src/MyLib.hs
+++ src/MyLib.hs
@@ -1,7 +1,10 @@
 module MyLib (someFunc, runHCat) where
 
 import qualified System.Environment as Env
-
+import Control.Monad
+import System.FilePath
+import Text.ParserCombinators.Parsec
+import Data.Text
 
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
@@ -26,4 +29,40 @@ handleArgs =
      [] -> Left "Error: No arguments provided!"
      _ -> Left "garbage detected"
 
-
+data FcsData30 = FcsData30 { -- punning
+  beginstext :: Int
+  , endstext :: Int
+  , begindata :: Int
+  , enddata :: Int
+  , beginanalysis :: Int
+  , endanalysis :: Int
+  , tot :: Int
+  , filver :: Int
+  , fil :: Text
+  , sys :: Text
+  , mode :: Char
+  , byteord :: [Int]
+  , datatype :: Char
+  , nextdata :: Int
+  , cytexpertfil :: Bool
+  , tbid :: Text -- should be UUID
+  , tbnm :: Text
+  , par :: Int
+  , btim :: Text -- should be time
+  , etim :: Text -- should be time
+  , date :: Text -- should be date
+  , cyt :: Text
+  , rctot :: Int
+  , usrctot :: Bool
+  , cgnm :: Text
+  , rctim :: Int
+  , usrctim :: Bool
+  , spillover :: Text -- should be complex table data structure
+  , rcvol :: Int
+  , usrcvol :: Bool
+   -- need to figure out how to add an arbitrary number of detectors with p#n, p#s, p#r, p#b, p#e, p#g
+  }
+{-
+parse x:xs |
+  x == "^L" = parse xs
+-}
