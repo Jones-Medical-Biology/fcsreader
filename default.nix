0d16f4732ea5c806d9c76ef4eaff1059be3ba847
Noah Thomas Jones <noahtjones@gmail.com>
2024-07-10 15:09:18 -0400

    added talk and nix script

---
+++ default.nix
@@ -0,0 +1,13 @@
+{ pkgs ? import <nixpkgs> {} }:
+
+pkgs.mkShell {
+  buildInputs = with pkgs; [
+    ghc
+    ghcid
+    stack
+    emacs-gtk
+    haskellPackages.cabal-install
+    haskellPackages.parsec
+    haskellPackages.filepath
+  ];
+}
